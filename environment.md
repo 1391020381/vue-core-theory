3ce726bd886dabac4e435077c10bc8c479184959
wangkaiwd <1162486176@qq.com>
2020-09-29 17:51:39 +0800

    feat: start project

---
+++ environment.md
@@ -0,0 +1,8 @@
+### 环境搭建
+1. 根目录创建`.gitignore`文件
+2. 安装项目依赖
+    ```shell script
+    yarn add webpack webpack-cli webpack-dev-server html-webpack-plugin -D
+    ```
+3. 根目录创建`webpack.config.js`，书写`webpack`配置
+4. `package.json`中创建启动脚本
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const absPath = (...dir) => path.resolve(__dirname, ...dir);
+console.log('path', [absPath('node_modules'), absPath('source')]);
+module.exports = {
+  mode: 'development',
+  // 入口文件
+  entry: './src/index.js',
+  // 出口文件
+  output: {
+    filename: 'bundle.js',
+    path: absPath('dist')
+  },
+  // 源码映射
+  devtool: 'source-map',
+  // 开发服务器:dev server
+  devServer: {
+    contentBase: './dist'
+  },
+  resolve: {
+    // 告诉webpack，在解析模块的时候，应该搜索哪个目录
+    modules: [absPath('node_modules'), absPath('source')]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html'
+    })
+  ]
+};
