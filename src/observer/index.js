4d42ff7868368ca77db63ddd0d6213503f4e3b4f
wangkaiwd <1162486176@qq.com>
2020-12-26 22:57:44 +0800

    feat: optimize code of defineProperty function

--- src/observer/index.js
+++ src/observer/index.js
@@ -6,7 +6,18 @@ function observe (data) {
 }
 
 function defineProperty (target, key) {
-  Object.defineProperty(target, key, {});
+  let value = target[key];
+  Object.defineProperty(target, key, {
+    get () {
+      return value;
+    },
+    set (newValue) {
+      if (newValue !== value) {
+        // 这样写会新将value指向一个新的值，而不会影响target[key]
+        value = newValue;
+      }
+    }
+  });
 }
 
 /**
@@ -20,22 +31,7 @@ class Observer {
   walk (data) {
     for (const key in data) {
       if (data.hasOwnProperty(key)) {
-        let value = data[key];
-        // get/set方法会更改范围内的value
-        Object.defineProperty(data, key, {
-          get () {
-            // 这样会造成死循环
-            // return data[key]
-            return value;
-          },
-          set (newValue) {
-            // 不能这样写，这样又会触发data中key对应属性的get方法
-            // data[key] = newValue;
-            if (newValue !== value) {
-              value = newValue;
-            }
-          }
-        });
+        defineProperty(data, key);
       }
     }
   }
