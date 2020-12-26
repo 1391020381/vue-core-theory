8bb30635536ac5b69ba3d1475dbc10b1f50f0b40
wangkaiwd <1162486176@qq.com>
2020-12-27 00:48:56 +0800

    feat: proxy property of data for vm

--- src/state.js
+++ src/state.js
@@ -21,10 +21,27 @@ function initMethods (vm) {
 
 }
 
+// 为vm代理所有data中的属性，可以直接通过vm.xxx来进行获取
+function proxy (target, key, source) {
+  Object.defineProperty(target, key, {
+    get () {
+      return source[key];
+    },
+    set (value) {
+      source[key] = value;
+    }
+  });
+}
+
 function initData (vm) {
   let data = vm.$options.data;
   vm._data = data = typeof data === 'function' ? data.call(vm) : data;
   observe(data);
+  for (const key in data) {
+    if (data.hasOwnProperty(key)) {
+      proxy(vm, key, data);
+    }
+  }
 }
 
 function initWatch () {
