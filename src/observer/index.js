e06cadf48afa2c6d4cd264167f7afee5da64cd1c
wangkaiwd <1162486176@qq.com>
2020-12-27 01:20:50 +0800

    feat: continue add todo

--- src/observer/index.js
+++ src/observer/index.js
@@ -38,6 +38,7 @@ class Observer {
     // 这里会对数组和对象进行单独处理，因为为数组中的每一个索引都设置get/set方法性能消耗比较大
     if (Array.isArray(value)) {
       // TODO: observeArray是Observer中的方法，并且需要为data添加__ob__来表示Observer实例，方便之后直接通过Vue中data中值来直接进行调用
+      // TODO: 将公共方法进行提取
       Object.setPrototypeOf(value, arrayProtoCopy);
       observeArray(value);
     } else {
